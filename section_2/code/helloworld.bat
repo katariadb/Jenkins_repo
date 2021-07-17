c50926b9793106d68762fba6173062aa2ba928b5
katariadb <84895602+katariadb@users.noreply.github.com>
2021-07-18 00:40:25 +0530

    Update helloworld.bat

--- section_2/code/helloworld.bat
+++ section_2/code/helloworld.bat
@@ -1,4 +1,4 @@
 @echo off
-echo "Hello World automati build trigger keep on trying again fedup      jghjgj gjhj       !!"
+echo "Hello World automati build trigger keep on trying again fedup     hj jghjgj gjhj       !!"
 pause
 4
