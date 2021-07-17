8c0a72177e0dad672d5c992401312796a93840dc
katariadb <84895602+katariadb@users.noreply.github.com>
2021-07-18 01:57:31 +0530

    sfsd

--- section_2/code/helloworld.bat
+++ section_2/code/helloworld.bat
@@ -1,3 +1,3 @@
 @echo off
-echo "Hello World automati build trigger keep on trying again fedu p 132 31zxcc dsf   !!"
+echo "Hello World automati build trigger keep on trying again fedu p 132 3zxc1zxcc dsf   !!"
 pause
