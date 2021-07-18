6e89e33383c7e9a05573819d41ea83c2fcffe71e
katariadb <84895602+katariadb@users.noreply.github.com>
2021-07-19 02:22:30 +0530

    Update helloworld.bat

--- section_2/code/helloworld.bat
+++ section_2/code/helloworld.bat
@@ -1,3 +1,3 @@
 @echo off
-echo "Hello World automati build trigger keep on txxryingkjk    fdgf sfgofdgd  !!"
+echo "Hello World automati build trigger keep on txx  ryingkjk    fdgf sfgofdgd  !!"
 pause
